ccd01ab08fe340dac68b025a27e21ce1f58c48e2
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2025-05-01 03:26:21 -0300

    Removendo arquivos desnecessarios

---
+++ compilador.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 *.cpp -o monster_shooter `pkg-config --cflags --libs opencv4`
--- monster.h
+++ monster.h
@@ -25,3 +25,92 @@ public:
 };
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class GameManager {
+    private:
+        - cv::Mat background;
+        - cv::Mat monsterImg;
+        - cv::VideoCapture cap;
+    
+        - unsigned int hordeNumber;
+        - unsigned int monstersKilled;
+        - unsigned int totalMonstersDefeated;
+        - const unsigned int MAX_HORDES;
+        - const unsigned int MAX_MONSTERS;
+        - const unsigned int FINAL_BOSS_HORDE;
+        - std::chrono::steady_clock::time_point roundStartTime;
+        - const int ROUND_DURATION_SECONDS;
+    
+        - unsigned int shotsFired;
+        - unsigned int missedShots;
+        - const unsigned int MAX_MISSED_SHOTS;
+    
+        - int hordeSize;
+        - int baseSpeed;
+        - bool finalBossSpawned;
+    
+        - cv::Scalar lowerGreen;
+        - cv::Scalar upperGreen;
+        - cv::Point2f currentAim;
+        - bool showCrosshair;
+    
+        - std::vector<Monster> monsters;
+        - bool gameRunning;
+        - bool gameWon;
+    
+    public:
+        + GameManager();
+        + ~GameManager();
+    
+        + bool initialize
+        + void run
+        + bool isGameRunning
+        + bool isGameWon
+        + unsigned int getHordeNumber
+        + unsigned int getMonstersKilled
+        + unsigned int getTotalMonstersDefeated
+        + unsigned int getMissedShots
+        + unsigned int getMaxMissedShots
+        + unsigned int getMaxHordes
+        + void saveHighScore
+        + int loadHighScore
+        + void showTransitionScreen
+        + bool showMenu
+        - void spawnHorde
+        - void spawnFinalBoss
+        - void processFrame
+        - void updateGameState
+        - void render
+        - void handleInput
+        - void drawCrosshair
+        - void fireShot
+        - void showVictoryScreen;
+        - void showDefeatScreen
+        - void showHordaInfo
+        - bool detectGreenObject
+    };
+    
